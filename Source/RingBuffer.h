4e639f7ad1e72a23c1d04c986f66a987afa24e57
KapJLKapJL <zolotnikolik@yandex.ru>
2022-09-12 19:50:37 +0300

    Added simple RingBuffer implementation

---
+++ Source/RingBuffer.h
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    RingBuffer.h
+    Created: 12 Sep 2022 7:18:33pm
+    Author:  User
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include<array>
+
+
+template
+<std::size_t size_>
+class RingBuffer
+{
+public:
+    RingBuffer() = default;
+    ~RingBuffer() = default;
+
+    void push(float sample)
+    {
+        m_samples[m_last_idx++] = sample;
+        m_last_idx %= size_;
+        if (m_last_idx == 0)
+            is_circle = true;
+    }
+
+    std::size_t size()
+    {
+        return is_circle ? size_ : m_last_idx;
+    }
+
+    float at(std::size_t idx)
+    {
+        return m_samples[(m_last_idx + 1 + idx) % size_];
+    }
+
+private:
+    std::array<float, size_> m_samples{ 0.f };
+    std::size_t m_last_idx{ 0 };
+
+    bool is_circle{ false };
+};
